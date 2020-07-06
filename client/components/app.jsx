fc0dad256a066c9c65cf3ee636c70f92de9e144e
jasethomson <jasethomson31@gmail.com>
2020-07-06 07:40:24 -0700

    Displays a title at the top of the user interface.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Header from './header';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: [],
+      appHeader: 'Student Grade Table'
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
-    return null;
+    const st = this.state;
+    return (
+      <Header title={st.appHeader}/>
+    );
   }
 }
