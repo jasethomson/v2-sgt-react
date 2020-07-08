c785fa0eb00e11fcbecafbab44c1ca96b591dca9
jasethomson <jasethomson31@gmail.com>
2020-07-07 21:44:37 -0700

    added badge for average grades

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
   return (
     <header>
       <h1>{props.title}</h1>
-      <h1>{props.gradeAvg}</h1>
+      <h1>Average Grade <span className="badge badge-secondary">{props.gradeAvg}</span></h1>
     </header>
   );
 }
