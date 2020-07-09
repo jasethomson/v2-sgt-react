9b5c9f9f47195cf425586d0f9aab2fb6e253552a
jasethomson <jasethomson31@gmail.com>
2020-07-08 19:02:43 -0700

    completed r1 of desktop styling

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -5,10 +5,10 @@ export default function Grade(props) {
     const tds = [];
     for (const attr in props.grade) {
       if (attr !== 'id') {
-        tds.push(<td key={props.grade[attr]}>{props.grade[attr]}</td>);
+        tds.push(<td className="tdText" key={props.grade[attr]}>{props.grade[attr]}</td>);
       }
     }
-    tds.push(<td key={`${props.grade.id}Delete`}><button onClick={() => props.deleteGrade(props.grade.id)}>Delete</button></td>);
+    tds.push(<td key={`${props.grade.id}Delete`}><button className="button tableButton" onClick={() => props.deleteGrade(props.grade.id)}>Delete</button></td>);
     return (
       <tr id={props.grade.id}>
         {tds}
